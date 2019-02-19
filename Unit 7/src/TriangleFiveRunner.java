e7dc721d7b748de3e516b318d73a133e47c8c0cc
school <maronj0597@my.sduhsd.net>
2019-02-19 11:12:54 -0800

    unit 7  2

--- Unit 7/src/TriangleFiveRunner.java
+++ Unit 7/src/TriangleFiveRunner.java
@@ -1,12 +1,11 @@
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
 
 import static java.lang.System.*;
 
-public class TriangleFiveRunner
-{
-   public static void main(String args[])
-   {
+public class TriangleFiveRunner {
+	public static void main(String args[]) {
 	}
 }
