6464b0afdefa42d323f7e77cc911a584769db432
school <maronj0597@my.sduhsd.net>
2019-02-25 10:00:46 -0800

    Testing Code

--- Test/src/Arrays.java
+++ Test/src/Arrays.java
@@ -5,5 +5,20 @@ public class Arrays {
 		int[] test = {1,2,3,4,5};
 		int length = (int) java.util.Arrays.stream(test).filter(i -> i % 2 == 0).count();
 		
+		int f=12, g=14;
+		if(((f>4) ||(++g>16)) && f>0) {
+		   System.out.println("dog");
+		}
+		System.out.println(g);
+		
+		int h=12, i=15;
+		if(h<4 ||++i>15 && h>8)   
+		{
+		   System.out.println("cat");
+		}
+		System.out.println(i);
+
+
+		
 	}
 }
