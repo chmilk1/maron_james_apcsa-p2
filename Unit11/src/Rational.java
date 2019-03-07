51439025359a94b891982a10b3519445696a74e7
school <maronj0597@my.sduhsd.net>
2019-03-07 11:06:12 -0800

    Attempted work on rational

--- Unit11/src/Rational.java
+++ Unit11/src/Rational.java
@@ -7,12 +7,22 @@ import static java.lang.System.*;
 class Rational implements Comparable<Rational>
 {
 	//add two instance variables
-
+	int numerator;
+	int denoninator;
 	//write two constructors
-
+	public Rational() {
+		this(1,1);
+	}
+	
+	public Rational(int num, int deno) {
+		setRational(num, deno);
+	}
 
 	//write a setRational method
-	
+	public void setRational(int num, int deno) {
+		this.numerator = num;
+		this.denoninator = deno;
+	}
 
 	//write  a set method for numerator and denominator
 
@@ -22,18 +32,31 @@ class Rational implements Comparable<Rational>
 		//num1/den1 + num2/den2 
 		//new numerator = (num1 * den2 + num2 * den1)
 		//new denominator = (den1 * den2)
+		numerator = other.getNumerator() + numerator;
+		denoninator = other.getDenoninator() + denoninator;
 		
-		
-
-
 		reduce();
+	}
+
+	public int getNumerator() {
+		return numerator;
+	}
+
+	public void setNumerator(int numerator) {
+		this.numerator = numerator;
+	}
+
+	public int getDenoninator() {
+		return denoninator;
+	}
+
+	public void setDenoninator(int denoninator) {
+		this.denoninator = denoninator;
 	}
 
 	private void reduce()
 	{
-
-
-
+		
 	}
 
 	private int gcd(int numOne, int numTwo)
