7cca4bdfe6c5ba23144f84f7117e12469a549352
school <maronj0597@my.sduhsd.net>
2019-05-01 10:35:15 -0700

    Starfighter added

---
+++ Unit17/src/MovingThing.java
@@ -0,0 +1,97 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public abstract class MovingThing implements Moveable
+{
+	private int xPos;
+	private int yPos;
+	private int width;
+	private int height;
+
+	public MovingThing()
+	{
+		xPos = 10;
+		yPos = 10;
+		width = 10;
+		height = 10;
+	}
+
+	public MovingThing(int x, int y)
+	{
+		xPos = x;
+		yPos = y;
+		width = 10;
+		height = 10;
+	}
+
+	public MovingThing(int x, int y, int w, int h)
+	{
+		setPos(x, y);
+		this.width = w;
+		this.height = h;
+	}
+
+	public void setPos( int x, int y)
+	{
+		this.xPos = x;
+		this.yPos = y;
+
+	}
+
+	public void setX(int x)
+	{
+		this.xPos = x;
+	}
+
+	public void setY(int y)
+	{
+		this.yPos = y;
+	}
+
+	public int getX()
+	{
+		return xPos;   //finish this method
+	}
+
+	public int getY()
+	{
+		return yPos;  //finish this method
+	}
+
+	public void setWidth(int w)
+	{
+		this.width = w;
+	}
+
+	public void setHeight(int h)
+	{
+		this.height = h;
+	}
+	
+	public void setDimentions(int w,int h) {
+		this.width = w;
+		this.height = h;
+	}
+
+	public int getWidth()
+	{
+		return width;  //finish this method
+	}
+
+	public int getHeight()
+	{
+		return height;  //finish this method
+	}
+
+	public abstract void move(String direction);
+	public abstract void draw(Graphics window);
+
+	public String toString()
+	{
+		return getX() + " " + getY() + " " + getWidth() + " " + getHeight();
+	}
+}
